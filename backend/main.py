bbebf93b9c5099e628d69c2f00fe3950b5414b59
Horl-Socheath <horl.socheath@gmail.com>
2022-11-26 18:31:36 +0700

    init database

--- backend/main.py
+++ backend/main.py
@@ -1,19 +1,31 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from core.config import settings
 from apis.general_pages.route_homepage import general_pages_router
+from db.session import engine
+from db.base import Base
 
 
-def include_router(app):
+def include_router(app: FastAPI):
   app.include_router(general_pages_router)
 
 
-def start_application():
+def configure_static(app: FastAPI):
+  app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+def create_table():
+  Base.metadata.create_all(bind=engine)
+
+
+def start_application() -> FastAPI:
   app = FastAPI(
     title=settings.PROJECT_NAME, 
     version=settings.PROJECT_VERSION
   )
   include_router(app=app)
-
+  configure_static(app=app)
+  create_table()
   return app
 
 
