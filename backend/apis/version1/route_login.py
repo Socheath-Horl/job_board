809a7d401561099b61d0d30d8f1e7475df933932
Horl-Socheath <horl.socheath@gmail.com>
2022-11-27 18:48:15 +0700

    add authentication endpont

---
+++ backend/apis/version1/route_login.py
@@ -0,0 +1,49 @@
+from datetime import timedelta
+
+from core.config import settings
+from core.hashing import Hasher
+from core.security import create_access_token
+from db.models.user import User
+from db.repository.login import get_user
+from db.session import get_db
+from fastapi import APIRouter
+from fastapi import Depends
+from fastapi import HTTPException
+from fastapi import status
+from fastapi.security import OAuth2PasswordRequestForm
+from schemas.token import Token
+from sqlalchemy.orm import Session
+
+
+router = APIRouter()
+
+
+def authenticate_user(username: str, password: str, db: Session):
+    user: User = get_user(username=username, db=db)
+    print(user)
+    if not user:
+        return False
+    if not Hasher.verify_password(
+        plain_password=password, hashed_password=user.hashed_password
+    ):
+        return False
+    return user
+
+
+@router.post("/token", response_model=Token)
+def login_for_access_token(
+    form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)
+):
+    user: User = authenticate_user(
+        username=form_data.username, password=form_data.password, db=db
+    )
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+        )
+    access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.email}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
