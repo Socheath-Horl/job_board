1273f656cf8815a71b7d20b6e52bf7e577ba63cc
Horl-Socheath <horl.socheath@gmail.com>
2022-11-28 20:50:40 +0700

    add job detail view

--- backend/webapps/base.py
+++ backend/webapps/base.py
@@ -4,3 +4,4 @@ from webapps.jobs import route_jobs
 
 api_router = APIRouter()
 api_router.include_router(router=route_jobs.router, prefix="", tags=["job-webapp"])
+api_router.include_router(router=route_jobs.router, prefix="/jobs", tags=["job-webapp"])
--- backend/webapps/jobs/route_jobs.py
+++ backend/webapps/jobs/route_jobs.py
@@ -1,4 +1,5 @@
 from db.repository.job import list_jobs
+from db.repository.job import retreive_job
 from db.session import get_db
 from fastapi import APIRouter
 from fastapi import Depends
@@ -17,4 +18,12 @@ async def home(request: Request, db: Session = Depends(get_db)):
     print(jobs)
     return templates.TemplateResponse(
         "general_pages/homepage.html", {"request": request, "jobs": jobs}
+    )
+
+
+@router.get("/detail/{id}")
+def job_detail(id: int, request: Request, db: Session = Depends(get_db)):
+    job = retreive_job(id=id, db=db)
+    return templates.TemplateResponse(
+        "jobs/detail.html", {"request": request, "job": job}
     )
