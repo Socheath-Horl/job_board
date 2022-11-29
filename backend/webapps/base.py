5a67e6b7d8f4ddf660b9731d5d25758ef8efe6ed
Horl-Socheath <horl.socheath@gmail.com>
2022-11-29 22:08:33 +0700

    add login page

--- backend/webapps/base.py
+++ backend/webapps/base.py
@@ -1,9 +1,13 @@
 from fastapi import APIRouter
+from webapps.auth import route_login
 from webapps.jobs import route_jobs
 from webapps.users import route_users
 
-
 api_router = APIRouter()
 api_router.include_router(router=route_jobs.router, prefix="", tags=["job-webapp"])
-api_router.include_router(router=route_jobs.router, prefix="/jobs", tags=["job-webapp"])
-api_router.include_router(router=route_users.router, prefix="", tags=["user-webapp"])
+api_router.include_router(
+    router=route_users.router, prefix="/user", tags=["user-webapp"]
+)
+api_router.include_router(
+    router=route_login.router, prefix="/auth", tags=["auth-webapp"]
+)
