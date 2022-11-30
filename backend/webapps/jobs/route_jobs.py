e76448ec91fd47f6fd568b1c18b4e303872c7747
Horl-Socheath <horl.socheath@gmail.com>
2022-11-30 21:21:56 +0700

    add create job view

--- backend/webapps/jobs/route_jobs.py
+++ backend/webapps/jobs/route_jobs.py
@@ -1,11 +1,19 @@
+from apis.version1.route_login import get_current_user_from_token
+from db.models.user import User
+from db.repository.job import create_new_job
 from db.repository.job import list_jobs
 from db.repository.job import retreive_job
 from db.session import get_db
 from fastapi import APIRouter
 from fastapi import Depends
 from fastapi import Request
+from fastapi import responses
+from fastapi import status
+from fastapi.security.utils import get_authorization_scheme_param
 from fastapi.templating import Jinja2Templates
+from schemas.job import JobCreate
 from sqlalchemy.orm import Session
+from webapps.jobs.forms import JobCreateForm
 
 
 templates = Jinja2Templates(directory="templates")
@@ -26,3 +34,31 @@ def job_detail(id: int, request: Request, db: Session = Depends(get_db)):
     return templates.TemplateResponse(
         "jobs/detail.html", {"request": request, "job": job}
     )
+
+
+@router.get("/post-a-job/")
+def get(request: Request, db: Session = Depends(get_db)):
+    return templates.TemplateResponse("jobs/create_job.html", {"request": request})
+
+
+@router.post("/post-a-job/")
+async def post(request: Request, db: Session = Depends(get_db)):
+    form = JobCreateForm(request=request)
+    await form.load_data()
+    if form.is_valid():
+        try:
+            token = request.cookies.get("access_token")
+            scheme, param = get_authorization_scheme_param(token)
+            current_user: User = get_current_user_from_token(token=param, db=db)
+            job = JobCreate(**form.__dict__)
+            job = create_new_job(job=job, db=db, owner_id=current_user.id)
+            return responses.RedirectResponse(
+                url=f"/detail/{job.id}", status_code=status.HTTP_302_FOUND
+            )
+        except Exception as e:
+            print(e)
+            form.__dict__.get("errors").append(
+                "You might not be logged in, In case problem persists please contact us."
+            )
+            return templates.TemplateResponse("jobs/create_job.html", form.__dict__)
+    return templates.TemplateResponse("jobs/create_job.html", form.__dict__)
